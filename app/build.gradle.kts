923761560f6b665cb85bab00b7eec96ed143cf62
Soner Demirci <soner.demirci10@gmail.com>
2025-05-02 19:13:23 +0300

    gradle and sdk version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.sonerdmrci.tictactoe"
-    compileSdk = 34
+    compileSdk = 35
 
     buildFeatures {
         viewBinding = true
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.sonerdmrci.tictactoe"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
